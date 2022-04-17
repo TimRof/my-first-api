a041eb1868a5b40ed981c06d15cff7afdd8f3b64
TimRof <TimRof@users.noreply.github.com>
2022-04-17 05:54:21 +0200

    Added exception handling for resource not found

--- src/main/java/nl/inholland/myfirstapi/controller/GuitarController.java
+++ src/main/java/nl/inholland/myfirstapi/controller/GuitarController.java
@@ -1,5 +1,6 @@
 package nl.inholland.myfirstapi.controller;
 
+import nl.inholland.myfirstapi.exception.ResourceNotFoundException;
 import nl.inholland.myfirstapi.model.Guitar;
 import nl.inholland.myfirstapi.service.GuitarService;
 import org.springframework.http.HttpStatus;
@@ -7,58 +8,66 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/guitars")
-public class GuitarController extends Controller {
+public class GuitarController extends Controller
+{
 
     private final GuitarService service;
 
-    public GuitarController(GuitarService service) {
+    public GuitarController(GuitarService service)
+    {
         this.service = service;
     }
 
     @GetMapping
     public @ResponseBody
-    List<Guitar> getAll() {
+    List<Guitar> getAll()
+    {
+
         return this.service.getAll();
     }
 
     @GetMapping(value = "/{id}")
     public @ResponseBody
-    ResponseEntity<?> getOne(@PathVariable long id) {
-        Optional<Guitar> guitar = this.service.getOne(id);
-        if (guitar.isPresent()) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(guitar);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    ResponseEntity<Guitar> getOne(@PathVariable long id)
+    {
+        Guitar guitar = this.service.getOne(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Guitar with id: " + id + " not found"));
+        return new ResponseEntity<>(guitar, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}")
     public @ResponseBody
-    ResponseEntity<?> deleteOne(@PathVariable long id) {
-        try {
+    ResponseEntity<Guitar> deleteOne(@PathVariable long id)
+    {
+        try
+        {
             this.service.deleteOne(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping(value = "/{id}")
     public @ResponseBody
-    ResponseEntity<?> updateOne(@RequestBody Guitar guitar, @PathVariable long id) {
-        Guitar _guitar = this.service.updateOne(guitar, id);
-        if (_guitar != null) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(_guitar);
+    ResponseEntity<Guitar> updateOne(@RequestBody Guitar guitar, @PathVariable long id)
+    {
+        Guitar newGuitar = this.service.updateOne(guitar, id);
+        if (newGuitar == null)
+        {
+            throw new ResourceNotFoundException("Guitar with id: " + id + " not found");
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newGuitar);
     }
 
     @PostMapping()
     public @ResponseBody
-    Guitar makeOne(@RequestBody Guitar guitar) {
+    Guitar makeOne(@RequestBody Guitar guitar)
+    {
         return this.service.makeOne(guitar);
     }
 }
--- src/main/java/nl/inholland/myfirstapi/service/GuitarService.java
+++ src/main/java/nl/inholland/myfirstapi/service/GuitarService.java
@@ -8,40 +8,48 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class GuitarService {
+public class GuitarService
+{
 
     GuitarRepository guitarRepository;
 
-    public GuitarService(GuitarRepository guitarRepository) {
+    public GuitarService(GuitarRepository guitarRepository)
+    {
         this.guitarRepository = guitarRepository;
         guitarRepository.save(new Guitar("Gibson", "Les Paul"));
         guitarRepository.save(new Guitar("Fender", "Vintera"));
     }
 
-    public List<Guitar> getAll() {
+    public List<Guitar> getAll()
+    {
         return this.guitarRepository.findAll();
     }
 
-    public Optional<Guitar> getOne(long id) {
+    public Optional<Guitar> getOne(long id)
+    {
         return this.guitarRepository.findById(id);
     }
 
-    public Guitar makeOne(Guitar guitar) {
+    public Guitar makeOne(Guitar guitar)
+    {
         return this.guitarRepository.save(guitar);
     }
 
-    public Guitar updateOne(Guitar guitar, long id) {
+    public Guitar updateOne(Guitar guitar, long id)
+    {
         Optional<Guitar> guitarObj = this.getOne(id);
-        if (guitarObj.isPresent()) {
-            Guitar _guitar = guitarObj.get();
-            _guitar.setBrand(guitar.getBrand());
-            _guitar.setModel(guitar.getModel());
-            return this.guitarRepository.save(_guitar);
+        if (guitarObj.isEmpty())
+        {
+            return null;
         }
-        return null;
+        Guitar newGuitar = guitarObj.get();
+        newGuitar.setBrand(guitar.getBrand());
+        newGuitar.setModel(guitar.getModel());
+        return this.guitarRepository.save(newGuitar);
     }
 
-    public void deleteOne(long id) {
+    public void deleteOne(long id)
+    {
         this.guitarRepository.deleteById(id);
     }
 }
